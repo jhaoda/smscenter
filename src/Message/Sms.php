d83a03b83b46c8ccff61feef7a09d3e2f616d0b7
JhaoDa <jhaoda@gmail.com>
2015-09-09 05:28:31 +0700

    Возможность добавить комментарий в sms-сообщение

--- src/Message/Sms.php
+++ src/Message/Sms.php
@@ -9,8 +9,16 @@ class Sms extends AbstractMessage implements TransliterableContract
 {
     use Transliterable;
 
-    public $comment;
+    /**
+     * @type string комментарий в sms-сообщении
+     */
+    private $comment;
 
+    /**
+     * Тип сообщения: sms, mms, ping и т.д.
+     *
+     * @type int
+     */
     private $type;
 
     public function __construct($phones, $message = null, $type = self::TYPE_SMS)
@@ -19,6 +27,36 @@ class Sms extends AbstractMessage implements TransliterableContract
 
         $this->message = $message;
         $this->type    = $type;
+    }
+
+    /**
+     * Добавление комментария.
+     *
+     * @param  string  $comment
+     *
+     * @return $this
+     */
+    public function setComment($comment)
+    {
+        if ($this->type == AbstractMessage::TYPE_SMS) {
+            $this->comment = $comment;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function toArray()
+    {
+        $params = parent::toArray();
+
+        if ($this->type == AbstractMessage::TYPE_SMS && $this->comment) {
+            $params['mes'] .= "\n~~~\n".$this->comment;
+        }
+
+        return $params;
     }
 
     public function getType()
